54836f9a5ea5bf529d040a5ebd24b7ed246c76e9
Asromain <mesprojetsinfo@gmail.com>
2014-03-22 12:37:51 +0100

    Tests d'integration (suite) en utilisant selector

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,24 +1,54 @@
 require 'spec_helper'
 describe "Static pages" do
 
+  # TEST INTEGRATION WITHOUT SELECTOR
   describe "Home page" do
     it "should have the content 'Example App'" do
       visit '/static_pages/home'
       page.should have_content('Example App')
     end
   end
-
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
   end
-
   describe "About page" do
     it "should have the content 'A propos (about)'" do
       visit '/static_pages/about'
       page.should have_content('A propos (about)')
+    end
+  end
+
+  # TEST INTEGRATION USING SELECTORS
+  describe "All pages with selectors" do 
+    # pour page Home
+    it "should have right title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Example App | Home")
+    end
+    it "should have the h1 'Example App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Example App')
+    end
+    # pour page Help
+    it "should have right title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Example App | Help")
+    end
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+    # pour page About
+    it "should have right title 'A propos (about)'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Example App | About")
+    end
+    it "should have the h1 'A propos (about)'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'A propos (about)')
     end
   end
 
