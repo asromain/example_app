d6d1596ec699bbcabb11afaa6cf5a0df72ed22d7
Asromain <mesprojetsinfo@gmail.com>
2014-04-04 10:25:21 -0700

    Ajout de liens vers mes pages dans le header et footer

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,32 +1,33 @@
 require 'spec_helper'
 
+
 describe "Static pages" do
 
 	subject { page }
 
 	describe "Home page" do
 		before { visit root_path }
-		it { should have_selector('h1',    text: 'Home') }
-		it { should have_selector('title', text: 'Ruby on Rails Tutorial New App') }
-		it { should_not have_selector('title', text: 'Home') }
+	    it { should have_selector('h1',    text: 'Home') }
+	    it { should have_selector('title', text: 'Ruby on Rails Tutorial New App') }
+	    it { should_not have_selector 'title', text: '| Home' }
 	end
 
 	describe "Help page" do
 		before { visit help_path }
 		it { should have_selector('h1',    text: 'Help') }
-		it { should have_selector('title', text: 'Ruby on Rails Tutorial New App') }
+		it { should have_selector('title', text: 'Ruby on Rails Tutorial New App | Help') }
 	end
 
 	describe "About page" do
 		before { visit about_path }
 		it { should have_selector('h1',    text: 'About') }
-		it { should have_selector('title', text: 'Ruby on Rails Tutorial New App') }
+		it { should have_selector('title', text: 'Ruby on Rails Tutorial New App | About') }
 	end
 
 	describe "Contact page" do
 		before { visit contact_path }
 		it { should have_selector('h1',    text: 'Contact') }
-		it { should have_selector('title', text: 'Ruby on Rails Tutorial New App') }
+		it { should have_selector('title', text: 'Ruby on Rails Tutorial New App | Contact') }
 	end
 
 
