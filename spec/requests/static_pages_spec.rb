6e72d781b52895fe5e16e60e6fed5e90886f5d70
Asromain <mesprojetsinfo@gmail.com>
2014-04-04 11:59:41 -0700

    Petites modifications d'agencement des pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
 
 	describe "Home page" do
 		before { visit root_path }
-	    it { should have_selector('h1',    text: 'Home') }
+	    it { should have_selector('h1',    text: 'My App') }
 	    it { should have_selector('title', text: 'Ruby on Rails Tutorial New App') }
 	    it { should_not have_selector 'title', text: '| Home' }
 	end
