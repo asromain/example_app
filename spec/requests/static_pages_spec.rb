1e8572cf6ca581442c359fc7bf603d43cd974d03
Asromain <mesprojetsinfo@gmail.com>
2014-04-04 09:59:57 -0700

    Compacité des tests d'intégration, et petits correctifs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,86 +2,59 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  # TEST INTEGRATION WITHOUT SELECTOR
-  describe "Home page" do
-    it "should have the content 'Home'" do
-      visit '/static_pages/home'
-      page.should have_content('Home')
-    end
-  end
-  describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
-  end
-  describe "About page" do
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      page.should have_content('About')
-    end
-  end
+	subject { page }
 
-  # TEST INTEGRATION USING SELECTORS
-  #  pour page Home
-  describe "Home page" do 
-    it "should have right title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial New App")
-    end
-    it "should have the h1 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Home')
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
-  end
-  #  pour page Help
-  describe "Help page" do 
-    it "should have right title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial New App")
-    end
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/help'
-      page.should_not have_selector('title', :text => '| Help')
-    end
-  end
-  #  pour page About
-  describe "About page" do 
-    it "should have right title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial New App")
-    end
-    it "should have the h1 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About')
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/about'
-      page.should_not have_selector('title', :text => '| About')
-    end
-  end
-  #  pour page Contact
-  describe "Contact" do 
-    it "should have right title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial New App")
-    end
-    it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/contact'
-      page.should_not have_selector('title', :text => '| Contact')
-    end
-  end
+	describe "Home page" do
+		before { visit root_path }
+		it { should have_selector('h1',    text: 'Home') }
+		it { should have_selector('title', text: 'Ruby on Rails Tutorial New App') }
+		it { should_not have_selector('title', text: 'Home') }
+	end
+
+	describe "Help page" do
+		before { visit help_path }
+		it { should have_selector('h1',    text: 'Help') }
+		it { should have_selector('title', text: 'Ruby on Rails Tutorial New App') }
+	end
+
+	describe "About page" do
+		before { visit about_path }
+		it { should have_selector('h1',    text: 'About') }
+		it { should have_selector('title', text: 'Ruby on Rails Tutorial New App') }
+	end
+
+	describe "Contact page" do
+		before { visit contact_path }
+		it { should have_selector('h1',    text: 'Contact') }
+		it { should have_selector('title', text: 'Ruby on Rails Tutorial New App') }
+	end
+
+
+  # @ ANCIENS EXEMPLES @
+
+  # TEST INTEGRATION WITHOUT SELECTOR
+  # describe "Home page" do
+  #   it "should have the content 'Home'" do
+  #     visit '/static_pages/home'
+  #     page.should have_content('Home')
+  #   end
+  # end
+
+  # # TEST INTEGRATION USING SELECTORS
+  # #  pour page Home
+  # describe "Home page" do 
+  #   it "should have right title 'Home'" do
+  #     visit '/static_pages/home'
+  #     page.should have_selector('title', :text => "Ruby on Rails Tutorial New App")
+  #   end
+  #   it "should have the h1 'Home'" do
+  #     visit '/static_pages/home'
+  #     page.should have_selector('h1', :text => 'Home')
+  #   end
+  #   it "should not have a custom page title" do
+  #     visit '/static_pages/home'
+  #     page.should_not have_selector('title', :text => '| Home')
+  #   end
+  # end
 
 end
