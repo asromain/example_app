63a7fbcadcfa014a179fc09223426fb893598dba
Asromain <mesprojetsinfo@gmail.com>
2014-04-17 22:14:57 +0200

    Ajout du controleur et des routes pour users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ExampleAppBis::Application.routes.draw do
+  get "users/new"
+
   #get "static_pages/contact"
   #get "static_pages/about"
   #get "static_pages/home"
@@ -8,6 +10,10 @@ ExampleAppBis::Application.routes.draw do
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+
+  resources :users
+  root to: 'static_pages#home'
+  match '/signup', to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
