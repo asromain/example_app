48e9d4205bf0ac21066379d5561677d5335b3d51
Asromain <mesprojetsinfo@gmail.com>
2014-04-28 06:46:03 -0700

    Ajout du formulatire d'inscription, ainsi que les tests et le style css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
 
-  # page show.html.erb
-  def show   
-  	@user = User.find(params[:id])
-  end
+# page show.html.erb
+	def show   
+		@user = User.find(params[:id])
+	end
 
-  # page new.html.erb
-  def new
-  	@title = "S'inscrire"
-  end
+# page new.html.erb
+	def new
+		@title = "S'inscrire"
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])  
+		if @user.save
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
   
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,11 +10,38 @@ describe "User pages" do
 		it { should have_selector('title', text: "Inscription") }
 	end
 
-	describe "profile page" do
+	describe "Profile page" do
 		let(:user) { FactoryGirl.create(:user) }
 		before { visit user_path(user) }
 
 		it { should have_selector('h1',    text: user.name) }
 		it { should have_selector('title', text: user.name) }
 	end
+
+	describe "Signup" do
+
+		before { visit signup_path }
+
+		let(:submit) { "Creer mon compte" }
+
+		describe "Avec information invalide" do
+			it "sans creer un nouvel utilisateur avec .not_to" do
+				expect { click_button submit }.not_to change(User, :count)
+			end
+		end
+
+		describe "Avec information valide" do
+			before do
+				fill_in "Name", 		with: "Exemple"
+				fill_in "Email",		with: "exemple@user.com"
+				fill_in "Password",		with: "111111"
+				fill_in "Confirmation",	with: "111111"
+			end
+
+			it "doit creer un compte" do
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+		end
+	end
 end
+
