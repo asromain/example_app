7258b888aa70e5c1b86dbe13a920370e37435b55
Asromain <mesprojetsinfo@gmail.com>
2014-05-07 10:38:45 -0700

    Modification de style

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
 	# Retourne le Gravatar pour un utilisateur donné
-	def gravatar_for(user, options = { size: 200} )
+	def gravatar_for(user, options = { size: 50 } )
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		size = options[:size]
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
