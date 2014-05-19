845239ac192e6f71669af7c69e1e5f63fb29dae9
Asromain <mesprojetsinfo@gmail.com>
2014-05-19 10:06:38 -0700

    Tests valides jusqu'à 11.2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,8 @@ describe "User pages" do
 
   it { should respond_to(:relationships) }
   it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
 
@@ -49,6 +51,11 @@ describe "User pages" do
 
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
 
     describe "and unfollowing" do
       before { @user.unfollow!(other_user) }
