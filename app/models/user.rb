7eecc526cdb0400f80d02a4ba1708744fdab0193
Asromain <mesprojetsinfo@gmail.com>
2014-05-19 10:04:31 -0700

    Ajout de nouveaux tests 11.15 non compris

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ActiveRecord::Base
   
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -37,6 +38,18 @@ class User < ActiveRecord::Base
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,9 +32,31 @@ describe "User pages" do
   it { should respond_to(:feed) }
 
   it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
 
   it { should be_valid }
   it { should_not be_admin }
+
+  # Following
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
 
   # Pour admin
   describe "with admin attribute set to 'true'" do
